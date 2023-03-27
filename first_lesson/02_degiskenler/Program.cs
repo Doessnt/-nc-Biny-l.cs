c2b14a3cc5d86bfab8c4c845ed46d45b15e2f445
Hpcodecraftt <hpcodecraftt@gmail.com>
2023-03-27 17:43:12 +0300

    C# ilk ders

---
+++ first_lesson/02_degiskenler/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Satır satır yazdırmak için
+            //Console.WriteLine("Hitler was right");
+
+            /* Yan yana yazdırmak için
+            Console.Write("Hitler was right");
+            Console.Write("Hitler was right");
+            */
+            /*
+             STRING KACIS KARAKTERLERI
+              
+             \n : alt satıra geçmeyi sağlar
+             \t : 4 boşluk bırakır 
+           
+            Console.WriteLine("hitler was\n RIGHT!!!");
+            Console.WriteLine("hitler was\t RIGHT!!!");
+              */
+            // *** DEĞİŞKENLER *** //
+                
+                 
+
+            // VeriTipi isimi = Değeri
+
+            // *** VERİ TİPLERİ *** //
+            /*
+                Sayısal veri tipleri(INT);
+                    int : +/- 10 basamağa kadar tam sayılar tanımlar.
+                    long : +/- 10+ basamağa kadar tam sayılar tanımlar.
+                    float: ondalık sayılar tutar.(Değer sonunda 'f' koymalısın)
+                    double: same sh*t 
+                    decimal: Bütün sayı tiplerini tanımlar. Değer sonuna 'm' eklenir
+                    
+                    Örnekler;
+                    int yaş = 18;
+                    float sayi = 12.5f;
+                    double sayi = 12.5;                            
+                    decimal asd = 123131231312m;
+                            
+             
+            */
+            /*
+                Sözel Veritipleri(STRING) char, string
+                    char: Tek karakter tanımlar
+                    string: Sonsuz uzunlukta metinsel değerler alabilir.
+                    
+                    Örnekler;
+                         char harf = 'A';
+                         char s = '1';
+                         char t = '.';
+                         string ad = "ADOLF HİTLER"
+                    
+             */
+             /*
+                Mantıksal Veritipi(Boolean):
+                     bool = sadece TRUE veya FALSE
+              
+              
+              */
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
---
+++ first_lesson/first_lesson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // comment line
+            /*
+               multiaple 
+                     comment 
+                            lines
+            */
+            /*
+             How to works a program ?
+             1- Build
+             2- Compile
+             3- DEBUG
+             4- EXECUTE
+             */
+            // EXAMPLE FOR ALGORTHM
+            /*
+             1 - Hazırlanmaya başla.
+             2- Eşyaları bul
+             3 - eşyalar listedekiler yanındamı ? T-> 4 F->2
+                   [tel, akbil, şarz, cüzdan, para]
+             4 - evden çık 
+             5 - metrobüs mü veya minibüsmü ?
+                    5.1 metrobüse doğru yürü 
+                        5.1.1 akbil bas
+                    5.2 minibüse doğru yürü
+                        5.2.1 minibüse bin ve para ver
+            6- durağa gelene kadar bekle 
+            7 - duraktan kuruma yürü
+            8 - exit()
+             */
+
+
+
+
+        }
+    }
+}
