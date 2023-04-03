91c40886cdd166ababb2944470471ea65263703e
Hpcodecraftt <hpcodecraftt@gmail.com>
2023-04-03 18:57:27 +0300

    HomeWork

---
+++ homeworks/ConsoleApp1/otomat/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otomat
+{
+    internal class Admin
+    {
+        public static void listProducts(string[] products,int[] prieces )
+        {
+            for(int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(i + " " + products[i] + " " + prieces[i] + " " + "TL");
+            }
+
+        }
+        public static void checkPrices(string[] products, int[] prices)
+        {
+            Console.Write("Lütfen Değiştirmek istediğiniz ürünü seçin(Ana menü için '5' )\n>>>>");
+            int user = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen değiştirmek istediğiniz fiyatı giriniz(Şuandaki fiyat {0})", prices[user]);
+            int user_change = Convert.ToInt32(Console.ReadLine());
+            prices[user] = user_change;
+            Console.Write("Değiştirldi...\nGüncel liste\n");
+            listProducts(products,prices);
+
+        }
+        public static void customer_buy(string[] products, int[] prices, int user, int user_input)
+        {
+            int userr = prices[user];
+            if (user_input < userr)
+            {
+                Console.WriteLine("Eksik para koydunuz\nLütfen tam fiyat değerinde para koyunuz");
+            }
+            else if (user_input > userr)
+            {
+                Console.WriteLine("Ürününüz verildi para üstünüz " + (user_input - userr) + "TL'dir");
+            }
+            else
+            {
+                Console.WriteLine("Ürününüz verildi");
+
+
+            }
+
+        }
+
+        public static void custemer_mechanic(string[] products, int[] prices)
+        {
+            listProducts(products,prices);
+            Console.WriteLine("Lütfen seçiminizi yapınız\n>>>>");
+            int user = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen parayıgiriniz\n>>>>");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+            user = prices[user];
+            customer_buy(products, prices, user, user_input);
+            
+        }
+
+
+    }
+}
---
+++ homeworks/ConsoleApp1/otomat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ürünler = { "Kola", "Bisküvi", "Çikolata" };
+            int[] fiyatlar = { 7, 3, 2 };
+
+            Console.WriteLine("Admin/Kullancı'mı olarak giriceksiniz ? \nAdmin -> 1\nÜye -> 2");
+            int user = Convert.ToInt32(Console.ReadLine());
+            if(user == 1)
+            {
+                Admin.listProducts(ürünler, fiyatlar);
+                Admin.checkPrices(ürünler, fiyatlar);
+            }
+            else if (user == 2)
+            {
+                Console.WriteLine("---Hoşgeldiniz---");
+                Admin.custemer_mechanic(ürünler, fiyatlar);
+
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
